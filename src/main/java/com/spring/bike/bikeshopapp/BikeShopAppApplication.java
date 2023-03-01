62b05ee3d1e81735f201e8fdb96b2a5a87d6db60
sshaparenko <shaparenko.archicgi@gmail.com>
2023-03-01 13:02:50 +0200

    app start files

---
+++ src/main/java/com/spring/bike/bikeshopapp/BikeShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bike.bikeshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeShopAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/bike/bikeshopapp/BikeShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bike.bikeshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
