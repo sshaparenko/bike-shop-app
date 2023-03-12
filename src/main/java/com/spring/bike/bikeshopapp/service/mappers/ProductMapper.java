e92fd59a1e552044a8e04f7eda6ac3cb47e02b5a
sshaparenko <shaparenko.archicgi@gmail.com>
2023-03-12 21:52:20 +0200

    product mapper

---
+++ src/main/java/com/spring/bike/bikeshopapp/service/mappers/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.spring.bike.bikeshopapp.service.mappers;
+
+import com.spring.bike.bikeshopapp.dto.product.CreateProductDTO;
+import com.spring.bike.bikeshopapp.model.Category;
+import com.spring.bike.bikeshopapp.model.Product;
+
+public interface ProductMapper {
+    default Product DTOtoProduct(CreateProductDTO createProductDTO, Category category) {
+        return Product.builder()
+                .name(createProductDTO.getName())
+                .price(createProductDTO.getPrice())
+                .description(createProductDTO.getDescription())
+                .inStorage(createProductDTO.isInStorage())
+                .category(category)
+                .build();
+    }
+}
